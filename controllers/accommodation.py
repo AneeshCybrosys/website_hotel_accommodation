897531de1f9734a8605be3d867a52a3bd6b2a0e1
AneeshCybrosys <aneeshmathapuzha@gmail.com>
2022-08-08 16:47:42 +0530

    odoo15 commit 01

--- controllers/accommodation.py
+++ controllers/accommodation.py
@@ -6,7 +6,8 @@ class WebsiteForm(http.Controller):
     @http.route(['/accommodation'], type='http', auth="user", website=True)
     def accommodation(self):
         partners = request.env['res.partner'].sudo().search([])
-        rooms = request.env['hotel.room'].sudo().search([('available', '=', True)])
+        rooms = http.request.env['hotel.room'].search([('available', '=', True)])
+        # rooms = request.env['hotel.room'].sudo().search([('available', '=', True)])
         values = {}
         values.update({
             'partners': partners,
