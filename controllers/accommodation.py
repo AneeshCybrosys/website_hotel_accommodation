44a288d53f887ab716fd26f8b0aae1f9b7fb2d72
AneeshCybrosys <aneeshmathapuzha@gmail.com>
2022-08-09 10:00:24 +0530

    odoo15 commit aug09

--- controllers/accommodation.py
+++ controllers/accommodation.py
@@ -3,11 +3,10 @@ from odoo.http import request
 
 
 class WebsiteForm(http.Controller):
-    @http.route(['/accommodation'], type='http', auth="user", website=True)
+    @http.route(['/accommodation'], type='http', auth="public", website=True)
     def accommodation(self):
         partners = request.env['res.partner'].sudo().search([])
-        rooms = http.request.env['hotel.room'].search([('available', '=', True)])
-        # rooms = request.env['hotel.room'].sudo().search([('available', '=', True)])
+        rooms = request.env['hotel.room'].sudo().search([('available', '=', True)])
         values = {}
         values.update({
             'partners': partners,
@@ -18,4 +17,15 @@ class WebsiteForm(http.Controller):
     @http.route(['/accommodation/submit'], type='http', auth="public",
                 website=True)
     def customer_form_submit(self, **post):
-        print(self.partners)
+        booking = request.env['hotel.accommodation'].sudo().create({
+            'partner_id': int(post.get('partner_id')),
+            'room_id': int(post.get('room_id')),
+            # 'room_id.available': int(post.get('room_id')),
+            'check_in_date': post.get('accommodation_date')
+        })
+        booking.room_id.available = False
+        vals = {
+            'booking': booking,
+        }
+        return request.render("website_hotel_accommodation.form_success", vals)
+
